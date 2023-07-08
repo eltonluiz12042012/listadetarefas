c088b47bdf5c66cf4171fb50db6c62e727ae7276
Elton Luiz <eltonluiz12042012@gmail.com>
2023-07-08 11:38:50 -0300

    Desenvolvendo funções de submit e limpar campo e adicionando style na página

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <h1>React</h1>
-    );
-}
 
+
+const App = () => {
+    const ENTER_KEY = 13;
+    const SCAPE_KEY = 27
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const erase = () => {
+        setValue('');
+    }
+
+    const submit = () => {
+        console.log(value)
+        setValue(value);
+        erase();
+    }
+
+    const onKeyDown = (event) => {
+        if (event.which === ENTER_KEY) {
+            submit();
+        } else if (event.which === SCAPE_KEY) {
+            erase();
+        }
+    }
+
+    return (
+        <section id="app" className="container">
+            <header>
+                <h1 className='title'>TODO</h1>
+            </header>
+            <section className="main">
+                <input className='new-todo' placeholder='O que precisa ser feito?' value={value}
+                    onChange={onChange} onKeyDown={onKeyDown} />
+            </section>
+        </section>
+    );
+};
 export default App;
